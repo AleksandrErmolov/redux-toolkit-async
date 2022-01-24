bc04962d3491305945819f895fe2f0fc332a8551
Aleksandr Ermolov <90044699+AleksandrErmolov@users.noreply.github.com>
2022-01-24 16:58:27 +0300

    add AsyncThunk

---
+++ src/features/post/postSlice.js
@@ -0,0 +1,54 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+import axios from 'axios'
+
+
+const initialState = {
+    posts: []
+}
+
+export const getPosts = createAsyncThunk(
+    'posts/getPosts',
+    async (_, { rejectWithValue, dispatch }) => {
+        const res = await axios.get('https://jsonplaceholder.typicode.com/posts')
+        dispatch(setPosts(res.data))
+    })
+
+
+export const deletePostById = createAsyncThunk(
+    'posts/removePostById',
+    async (id, { rejectWithValue, dispatch }) => {
+        await axios.delete(`https://jsonplaceholder.typicode.com/posts/${id}`)
+        dispatch(deletePost(id))
+    }
+)
+
+
+export const postSlice = createSlice({
+
+    name: 'posts',
+    initialState,
+    reducers: {
+        setPosts: (state, action) => {
+            state.posts = action.payload
+        },
+        deletePost: (state, action) => {
+            state.posts = state.posts.filter(post => post.id !== action.payload)
+        }
+    },
+
+    extraReducers: {
+        [getPosts.fulfilled]: () => console.log('fulfilled'),
+        [getPosts.pending]: () => console.log('fulfilled'),
+        [getPosts.rejected]: () => console.log('fulfilled'),
+        [deletePostById.fulfilled]: () => console.log('fulfilled'),
+        [deletePostById.pending]: () => console.log('fulfilled'),
+        [deletePostById.rejected]: () => console.log('fulfilled'),
+
+    }
+
+
+})
+
+
+export const { setPosts, deletePost } = postSlice.actions
+export default postSlice.reducer
